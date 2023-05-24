00da3a19d294c2615f66be3f06135729ee94e9c1
antonystar-dev <antonioamuniz@outlook.com>
2023-05-24 04:25:41 -0300

    Inserindo imgs

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 function App() {
   const name = 'Antonio'
+  const url='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/706px-Google_%22G%22_Logo.svg.png'
   return (
     <div className="App">
       <h2>Meu programa</h2>
       <p>Olá, {name}</p>
+      <img src={url} height="50px"alt="Imagem teste"/>
       </div>
     
   );
