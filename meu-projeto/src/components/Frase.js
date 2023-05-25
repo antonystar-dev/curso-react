624921088a02d793f74d578ac275eac92d06f9a3
antonystar-dev <antonioamuniz@outlook.com>
2023-05-25 02:54:02 -0300

    Inserindo CSS

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -1,7 +1,8 @@
+import styles from './Frase.module.css'
 function Frase(){
     return(
-        <div>
-            <p>Estou colocando um componentes dentro de outro e exportando para principal</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Estou colocando um componentes dentro de outro e exportando para principal</p>
         </div>
     )
 }
