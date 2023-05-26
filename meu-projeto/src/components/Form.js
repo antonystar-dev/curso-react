5a563a2ae233723e1493b062fcec5fa238545db2
antonystar-dev <antonioamuniz@outlook.com>
2023-05-25 23:43:43 -0300

    formularios useState

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,14 +1,37 @@
+//Esse setName e o use state permite eu pegar os dados que estão sendo alterados no form
+import {useState} from 'react'
 function Form(){
     function cadastrarUsuario(e){
         e.preventDefault()
+        console.log(name)
         console.log('Cadastrou usuario')
     }
+    const [name, setName]= useState()
+    const [password, setPassword]= useState()
     return(
         <div>
         <h1>Formulario de cadastro</h1>
         <form onSubmit={cadastrarUsuario}>
             <div>
-                <input type="text" placeholder="Digite o seu nome"/>
+                <label htmlFor="name">Nome:</label>
+                <input
+                 type="text"
+                  id="name"
+                   name="name"
+                    placeholder="Digite o seu nome"
+                //value={name} >>>>> aq to ja pegando os dados..... acho q posso usar no update, mas tenho q passar o nome no state
+                 onChange={(e) => setName(e.target.value)}
+                 />
+            </div>
+            <div>
+            <label htmlFor="password">Senha:</label>
+                <input
+                 type="password"
+                  id="password"
+                   nome="password"
+                    placeholder="Digite o sua senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
             </div>
             <div>
                 <input type="submit" value="Cadastrar"/>
